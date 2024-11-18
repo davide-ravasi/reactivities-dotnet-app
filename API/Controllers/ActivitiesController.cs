8a052599d0e1334850a571bf40bf363b7d057824
Davide Ravasi <dravasi@d-edge.com>
2024-11-18 14:50:25 +0100

    feat: added endpoint for activities and activity

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,7 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Persistence;
 
 namespace API.Controllers
@@ -9,9 +8,29 @@ namespace API.Controllers
     public class ActivitiesController : BaseApiController
     {
         private readonly DataContext _context;
+        // DataContext will be injected into the constructor
+        // This is called dependency injection
+        //  when an http request commes in, the controller will be created and the DataContext will be injected into it
         public ActivitiesController(DataContext context)
         {
             _context = context;
+
+        }
+
+        [HttpGet] //endpoint api/activities and it will return a list of activities
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")] //endpoint api/activities/id and it will return a single activity
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            return await _context.Activities.FindAsync(id);
         }
     }
 }
+
+// EF Core is an object-relational mapper (O/RM) that enables .NET developers to work with a database using .NET objects. It eliminates the need for most of the data-access code that developers usually need to write.
+// ToListAsync() is an extension method that executes the query and returns a list of the results from EF Core
+// FindAsync() is an extension method that finds an entity with the given primary key values. Returns the found entity or null if not found.
--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace API.Controllers
 {
     [ApiController] // This attribute indicates that the class defines an API controller that responds to HTTP requests
-    [Route("[controller]")] // [controller] is a token that is replaced by the controller class name minus the "Controller" suffix
+    [Route("api/[controller]")] // [controller] is a token that is replaced by the controller class name minus the "Controller" suffix
     public class BaseApiController : ControllerBase  // ControllerBase is a base class for an MVC controller without view support because we use React for the view
     {
 
