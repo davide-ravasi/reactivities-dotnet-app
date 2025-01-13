576e6d0f54cec1d73c7ba004f20e8cfcef947bfa
Davide Ravasi <dravasi@d-edge.com>
2025-01-13 10:40:11 +0100

    fix: added deleted activity

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f21b5ce20547ad3fb74ad066526084608d4fc64b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28cbf39cdb05cd5882eda97156538bd828b489c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
